8ab49a53cca96efb4b2db862a63b489076f25c32
nitantchhajed <nitant.chhajed@outlook.com>
2023-11-17 23:51:10 +0530

    varible declaration

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"
+ 
+	//After we initialised a variable with := then we don't have to use := again to assign a new value to it
+	//example ->
+	//cards = "Five of Diamonds" 
+	fmt.Println(card)
+}
