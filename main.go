55b233d578d7b0a24b8720e305917cb094f008ee
nitantchhajed <nitant.chhajed@outlook.com>
2023-11-17 23:58:58 +0530

    17

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
- 
+	card := newCard()
+
 	//After we initialised a variable with := then we don't have to use := again to assign a new value to it
 	//example ->
-	//cards = "Five of Diamonds" 
+	//cards = "Five of Diamonds"
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
